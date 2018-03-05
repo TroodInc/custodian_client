0efeaa2c8a88653918088f2afc457664a4f59d4b
Сергей Дубинин <whackojacko.ru@gmail.com>
2018-03-05 12:10:23 +0500

    TB-7: Testcase for testing Custodian exceptoin on improper update of object

--- custodian/tests/test_object.py
+++ custodian/tests/test_object.py
@@ -1,5 +1,8 @@
+import pytest
 from hamcrest import *
 
+from custodian.client import Client
+from custodian.exceptions import ObjectUpdateException
 from custodian.objects.fields import NumberField, StringField, BooleanField, RelatedObjectField
 from custodian.objects import Object
 
@@ -81,3 +84,34 @@ def test_object_with_related_outer_object_field_serializes_itself(client):
     expected_serialized_field = {'name': 'addresses', 'type': 'array', 'optional': False, 'linkMeta': 'address',
                                  'linkType': 'outer', 'outerLinkField': 'address_id'}
     assert_that(serialized_object['fields'][3], equal_to(expected_serialized_field))
+
+
+def test_improper_object_operations_cause_custodian_error(client: Client, flush_database):
+    assert_that(client.objects.get_all(), has_length(0))
+    person_obj = Object(
+        name='person',
+        key='id',
+        cas=False,
+        fields=[
+            NumberField(name='id', optional=True, default={'func': 'nextval'}),
+            StringField(name='street'),
+            BooleanField(name='house')
+        ],
+        objects_manager=client.objects
+    )
+    client.objects.create(person_obj)
+    assert_that(client.objects.get_all(), has_length(1))
+    with pytest.raises(ObjectUpdateException) as error:
+        person_obj = Object(
+            name='person',
+            key='id',
+            cas=False,
+            fields=[
+                NumberField(name='id', optional=True),
+                StringField(name='street'),
+                BooleanField(name='house')
+            ],
+            objects_manager=client.objects
+        )
+        client.objects.update(person_obj)
+    assert_that(error.value.args[0], contains_string('cannot drop sequence'))
