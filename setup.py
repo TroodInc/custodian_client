aa42675365f798d79f69fcb46d10ee5b6acc939f
Alexander Gor <ornot.work@gmail.com>
2018-08-06 15:09:58 +0300

    setup file fixed

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from os.path import join, dirname
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='custodian_client',
     version=0.1,
     long_description=open(join(dirname(__file__), 'README.md')).read(),
+    include_package_data=True,
+    packages=find_packages(),
     author='Trood CIS',
     url='',
     install_requires=[
